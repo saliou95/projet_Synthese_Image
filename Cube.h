650259ac887122945b82cccde2322b03d6fe06eb
Maureen <sojeba008@gmail.com>
2021-12-30 20:48:58 +0100

    ajout de la classe Cube

---
+++ Cube.h
@@ -0,0 +1,14 @@
+#ifndef DEF_CUBE
+#define DEF_CUBE
+#include "Primtv.h"
+
+class Cube: public Primtv
+{
+    public:
+        //    void init(GLfloat R,int nbPoints, float z0, float z1);
+           void init(GLfloat L,GLfloat l);
+    protected:
+            
+};
+
+#endif
